d6d21ef3520b3e7ff2e65e5f1058fb961a6c22eb
Seungha Jeon <seuha516@naver.com>
2022-10-18 20:25:00 +0900

    [SETTING] set dotenv
    환경변수를 가져오기 위해 dotenv 설정

--- backend/manage.py
+++ backend/manage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import os
-import sys
+import os, sys, dotenv
 
 
 def main():
@@ -19,4 +18,5 @@ def main():
 
 
 if __name__ == '__main__':
+    dotenv.read_dotenv()
     main()
