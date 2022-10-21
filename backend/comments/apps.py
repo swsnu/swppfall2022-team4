8cc6bdb6f20bc39fc14703c1fcb1bf12496dfe39
Seungha Jeon <seuha516@naver.com>
2022-10-21 16:47:14 +0900

    [SETTING] change app name

--- backend/comments/apps.py
+++ backend/comments/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GroupConfig(AppConfig):
+class CommentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'group'
+    name = 'comments'
