b3545834f495d2dc9327916a7edaafd18344102a
Seungha Jeon <seuha516@naver.com>
2022-10-23 19:07:15 +0900

    [FEAT] User 모델

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
-# class User(models.Model):
-#     username = models.CharField(max_length=20, null=False)
-#     nickname = models.CharField(max_length=255, null=False)
-#     gender = models.CharField(max_length=1, null=False)
-#     age = models.IntegerField(null=False)
-#     height = models.FloatField(null=False)
-#     weight = models.FloatField(null=False)
-#     point = models.IntegerField(blank=True, null=True)
+class User(models.Model):
+    username = models.CharField(max_length=20, null=False)
+    hashedPassword = models.CharField(max_length=255, null=False)
+    nickname = models.CharField(max_length=8, null=False)
+    gender = models.CharField(max_length=10, null=False)
+
+    age = models.IntegerField(null=False)
+    height = models.FloatField(null=False)
+    weight = models.FloatField(null=False)
+
+    image = models.CharField(max_length=255, null=False)
+
+    exp = models.IntegerField(null = False)
+    level = models.IntegerField(null = False)
