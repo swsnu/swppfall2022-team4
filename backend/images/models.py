d503bad21f3b1dccd405531fd9e9992e28c76e34
Seungha Jeon <seuha516@naver.com>
2022-10-23 20:56:39 +0900

    [FIX] max_length 변경

--- backend/images/models.py
+++ backend/images/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Image(models.Model):
-    title = models.CharField(max_length=16)
+    title = models.CharField(max_length=30)
     file = models.ImageField(upload_to="")
