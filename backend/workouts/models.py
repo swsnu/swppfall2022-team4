48d6d0f5cb5706f3483ca08678548622eca08cfd
Kojihyung <kokoleerh@naver.com>
2022-10-28 18:35:48 +0900

    [FEAT] workout 관련 백엔드 모델 생성

--- backend/workouts/models.py
+++ backend/workouts/models.py
@@ -1,8 +1,35 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
-# from users.models import User
+from users.models import User
 
-# class Workout(models.Model):
-#     author = models.ForeignKey(User, on_delete=models.CASCADE)
-#     year = models.IntegerField(null=False)
-#     month = models.IntegerField(null=False)
-#     date = models.IntegerField(null=False)
+
+class FitElement(models.Model):
+    TYPE_CHOICE = (
+        ('goal', 'goal'),
+        ('log', 'log')
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='fit_element')
+    type = models.CharField(
+        max_length=4, choices=TYPE_CHOICE, null=False)  # goal / log
+    workout_type = models.CharField(max_length=30, null=False)  # 운동종류
+    period = models.IntegerField(null=True)
+    category = models.CharField(max_length=30, null=True)  # 하체, 등 카테고리
+    weight = models.IntegerField(null=True)
+    rep = models.IntegerField(null=True)
+    set = models.IntegerField(null=True)
+    time = models.IntegerField(null=True)
+    date = models.DateField(null=True)
+
+
+class DailyLog(models.Model):
+    date = models.DateField(null=False)
+    memo = models.TextField(null=True)
+    fit_element = models.ManyToManyField(FitElement, blank=True)
+
+
+class Routine(models.Model):
+    name = models.CharField(max_lenght=30, null=False)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='routine')
+    fit_element = models.ManyToManyField(FitElement, blank=True)
