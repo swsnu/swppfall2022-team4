6294425aeca658f99abcbc092d40ac6c18fa19ee
KJYoung <jykim157@snu.ac.kr>
2022-10-28 21:30:45 +0900

    [FEAT] Comment, Post model created along with its admin

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,9 +1,25 @@
+from email.policy import default
 from django.db import models
-# from users.models import User
-# from posts.models import Post
+from core.models import AbstractTimeStampedModel
+from users.models import User
+from posts.models import Post
 
-# class Comment(models.Model):
-#     post = models.ForeignKey(Post, on_delete=models.CASCADE, db_index=True)
-#     author = models.ForeignKey(User, on_delete=models.CASCADE)
-#     content = models.TextField()
-#     date = models.DateTimeField(db_index=True)
+
+class Comment(AbstractTimeStampedModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+
+    content = models.TextField()
+
+    like_num = models.IntegerField(null=False, default=0)
+    dislike_num = models.IntegerField(null=False, default=0)
+
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, blank=True)
+
+    def get_eff_like(self):
+        """ Get effective number of like """
+        return self.like_num - self.dislike_num
+    
+    def __str__(self):
+        """ To string method """
+        return self.content
