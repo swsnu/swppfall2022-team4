06e69656566f338b02b412398550ecb9809be42d
Seungha Jeon <seuha516@naver.com>
2022-10-29 14:24:41 +0900

    [STYLE] code clean

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-# from users.models import User
-# from posts.models import Post
-
-# class Comment(models.Model):
-#     post = models.ForeignKey(Post, on_delete=models.CASCADE, db_index=True)
-#     author = models.ForeignKey(User, on_delete=models.CASCADE)
-#     content = models.TextField()
-#     date = models.DateTimeField(db_index=True)
--- backend/groups/views.py
+++ backend/groups/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponseNotAllowed, JsonResponse
-# from .models import Group
 
-# def group_list(request):
-#     if request.method == 'GET':
-#         group_list = [group for group in Group.objects.all()]
-#         return JsonResponse(group_list, safe=False)
-#     else:
-#         return HttpResponseNotAllowed(['GET'])
+# Create your views here.
