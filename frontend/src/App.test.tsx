282e0b4c2fca172d10007742dd9e686e576a333e
Seungha Jeon <seuha516@naver.com>
2022-10-29 12:35:46 +0900

    [TEST] dummy test

--- frontend/src/App.test.tsx
+++
@@ -1,5 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-test('test', () => {
-  expect(1).toBe(1);
-});
---
+++ frontend/src/containers/Main.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import Main from './Main';
+
+describe('TEST?', () => {
+  test('TEST1?', () => {
+    render(<Main />);
+    expect(1).toBe(1);
+  });
+});
