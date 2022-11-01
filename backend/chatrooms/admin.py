cddf05eeadf68bbd3cea12ae2e26ce240b7304df
Seungha Jeon <seuha516@naver.com>
2022-11-01 22:40:36 +0900

    [FEAT] model

--- backend/chatrooms/admin.py
+++ backend/chatrooms/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Chatroom, Message
 
-# Register your models here.
+admin.site.register(Chatroom)
+admin.site.register(Message)
--- backend/chatrooms/models.py
+++ backend/chatrooms/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from utils.models import AbstractTimeStampedModel
 from users.models import User
 
-class ChatRoom(models.Model):
-    username1 = models.CharField(max_length=20, null=True)
-    username2 = models.CharField(max_length=20, null=True)
+class Chatroom(models.Model):
+    username1 = models.CharField(max_length=20)
+    username2 = models.CharField(max_length=20)
 
 class Message(AbstractTimeStampedModel):
-    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    room = models.ForeignKey(Chatroom, on_delete=models.CASCADE)
     # group = models.ForeignKey(Group, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     content = models.CharField(max_length=255)
