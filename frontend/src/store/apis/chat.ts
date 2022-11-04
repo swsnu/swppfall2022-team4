1e038651cb6fa3a49ee16a01aab3e0238033c40b
Seungha Jeon <seuha516@naver.com>
2022-11-04 23:30:17 +0900

    [FEAT] slices

---
+++ frontend/src/store/apis/chat.ts
@@ -0,0 +1,26 @@
+import client from './client';
+import { userType } from './user';
+
+export const getChatroomList = async (username: string) => {
+  const response = await client.get<chatroomType[]>(`/api/chat/${username}/`);
+  return response.data;
+};
+export const getMessageList = async (roomId: string) => {
+  const response = await client.get<messageType[]>(`/api/chat/message/${roomId}/`);
+  return response.data;
+};
+
+export type chatroomType = {
+  id: number;
+  user: {
+    username: string;
+    nickname: string;
+    image: string;
+  } | null;
+};
+export type messageType = {
+  id: number;
+  author: userType | null;
+  content: string;
+  created: string;
+};
---
+++ frontend/src/store/slices/chat.ts
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AxiosError, AxiosResponse } from 'axios';
+import { put, call, takeLatest } from 'redux-saga/effects';
+import * as chatAPI from 'store/apis/chat';
+
+interface ChatState {
+  socket: any;
+  chatroomList: chatAPI.chatroomType[];
+  messageList: chatAPI.messageType[];
+  error: AxiosError | null;
+}
+const initialState: ChatState = {
+  socket: null,
+  chatroomList: [],
+  messageList: [],
+  error: null,
+};
+
+export const chatSlice = createSlice({
+  name: 'chat',
+  initialState,
+  reducers: {
+    setSocket: (state, { payload }) => {
+      state.socket = payload;
+    },
+    getChatroomList: (state, action: PayloadAction<string>) => {
+      state.chatroomList = [];
+      state.error = null;
+    },
+    getChatroomListSuccess: (state, { payload }) => {
+      state.chatroomList = payload;
+      state.error = null;
+    },
+    getChatroomListFailure: (state, { payload }) => {
+      state.chatroomList = [];
+      state.error = payload;
+      alert(payload.response?.data.message);
+    },
+    getMessageList: (state, action: PayloadAction<string>) => {
+      state.messageList = [];
+      state.error = null;
+    },
+    getMessageListSuccess: (state, { payload }) => {
+      state.messageList = payload;
+      state.error = null;
+    },
+    getMessageListFailure: (state, { payload }) => {
+      state.messageList = [];
+      state.error = payload;
+      alert(payload.response?.data.message);
+    },
+  },
+});
+export const chatActions = chatSlice.actions;
+
+function* getChatroomListSaga(action: PayloadAction<string>) {
+  try {
+    const response: AxiosResponse = yield call(chatAPI.getChatroomList, action.payload);
+    yield put(chatActions.getChatroomListSuccess(response));
+  } catch (error) {
+    yield put(chatActions.getChatroomListFailure(error));
+  }
+}
+function* getMessageListSaga(action: PayloadAction<string>) {
+  try {
+    const response: AxiosResponse = yield call(chatAPI.getMessageList, action.payload);
+    yield put(chatActions.getMessageListSuccess(response));
+  } catch (error) {
+    yield put(chatActions.getMessageListFailure(error));
+  }
+}
+
+export default function* chatSaga() {
+  yield takeLatest(chatActions.getChatroomList, getChatroomListSaga);
+  yield takeLatest(chatActions.getMessageList, getMessageListSaga);
+}
