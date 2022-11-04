8e4e3b6d0c00b55793044c06f9e49f7ad1ea1977
blackruby02 <whiteruby02@snu.ac.kr>
2022-11-04 13:59:39 +0900

    [FIX] model

--- backend/groups/models.py
+++ backend/groups/models.py
@@ -4,18 +4,17 @@ from workouts.models import FitElement
 
 class Group(models.Model):
     """ group definition """
-    group_name = models.CharField(max_lenth = 30, null = False)
+    group_name = models.CharField(max_length = 30, null = False)
     group_leader = models.ForeignKey(
-        User, on_delete = models.CASCADE, related_name = 'group', null = False)
-    members = models.ManyToManyField(User, related_name = 'group', null = True)
-    number = models. IntegerField(null = False)
+        User, on_delete = models.CASCADE, related_name = 'own_group', null = False)
+    members = models.ManyToManyField(User, related_name = 'group')
+    number = models. IntegerField(null = True)
     start_date = models.DateField(null = True)
     end_date = models.DateField(null = True)
-    description = models.TextField(blank = True)
+    description = models.TextField(blank = False)
     free = models.BooleanField(null = False)
-    deleted = models.BooleanField(null = False)
 
 class Goal(models.Model):
     """ group goal definition """
     group_id = models.ForeignKey(Group, on_delete = models.CASCADE, related_name = 'goal')
-    fit_element = models.ManyToManyField(FitElement, related_name = 'group_goal', null = True)
+    fit_element = models.ManyToManyField(FitElement, related_name = 'group_goal')
