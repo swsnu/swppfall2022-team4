550601b69496257061f9b9b9f4c889cdec597507
blackruby02 <whiteruby02@snu.ac.kr>
2022-11-06 08:06:26 +0900

    [FEAT] GroupDetail Frontend

---
+++ frontend/src/containers/group/GroupDetail.tsx
@@ -0,0 +1,125 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from 'index';
+import { useNavigate, useParams } from 'react-router';
+import styled from 'styled-components';
+import { FitElement } from 'components/fitelement/FitElement';
+import { groupActions } from 'store/slices/group';
+
+const GroupDetail = () => {
+  const { group_id } = useParams<{ group_id: string }>();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const group_detail = useSelector(({ group }: RootState) => group.groupDetail.groupdetail);
+  const groupDeleteStatus = useSelector(({ group }: RootState) => group.groupDelete);
+
+  useEffect(() => {
+    if (group_id) {
+      dispatch(groupActions.getGroupDetail({ group_id: group_id }));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (groupDeleteStatus) {
+      navigate('/group');
+      dispatch(groupActions.stateRefresh());
+    }
+  }, [groupDeleteStatus]);
+
+  const deleteOnClick = () => {
+    if (group_id) {
+      dispatch(
+        groupActions.deleteGroup({
+          group_id: group_id,
+        }),
+      );
+    }
+  };
+
+  return (
+    <Wrapper>
+      <GroupDetailHeader>
+        <div>{group_detail?.group_name}</div>
+        <div>{group_detail?.start_date}</div>
+        <div>{group_detail?.end_date}</div>
+        <span>최대 인원수</span>
+        <div>{group_detail?.number}</div>
+        <span>현재 인원수 추가해야합니다. </span>
+      </GroupDetailHeader>
+      <GroupDetailWrapper>
+        <h1>About</h1>
+        <span>그룹장</span>
+        <span>{group_detail?.group_leader.username}</span>
+        <span>닉네임</span>
+        <span>{group_detail?.group_leader.nickname}</span>
+        <h1>Specification</h1>
+        <span>기간 : </span>
+        <span>
+          {group_detail?.start_date}~{group_detail?.end_date}
+        </span>
+        <span>인원수 : </span>
+        <span>{group_detail?.number}</span>
+      </GroupDetailWrapper>
+      {group_detail?.goal.map((goal, index) => (
+        <FitElement
+          key={index}
+          id={index + 1}
+          type={goal.type}
+          workout_type={goal.workout_type}
+          category={goal.category}
+          weight={goal.weight}
+          rep={goal.rep}
+          set={goal.set}
+          time={goal.time}
+        />
+      ))}
+      <GroupDeleteBtn onClick={deleteOnClick}>그룹삭제하기</GroupDeleteBtn>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+`;
+
+const GroupDetailHeader = styled.div`
+  border: 1px solid black;
+  margin-right: 15px;
+`;
+
+const GroupDetailWrapper = styled.div`
+  border: 1px solid black;
+  margin-right: 15px;
+  width: 40%;
+  height: 100%;
+  min-height: 100%;
+  background-color: #ffffff;
+  position: relative;
+`;
+
+const JoinGroupBtn = styled.button`
+  width: 70px;
+  height: 30px;
+  margin: 5px;
+  background-color: #d7efe3;
+  border-radius: 15px;
+  font-size: 10px;
+  cursor: pointer;
+`;
+
+const GroupDeleteBtn = styled.button`
+  width: 70px;
+  height: 30px;
+  margin: 5px;
+  background-color: #d7efe3;
+  border-radius: 15px;
+  font-size: 10px;
+  cursor: pointer;
+`;
+
+export default GroupDetail;
