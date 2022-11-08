859d2e7d0164adc2e9d322ddfe5bc6854eb7e0ce
Seungha Jeon <seuha516@naver.com>
2022-11-09 00:50:50 +0900

    [TEST] login

--- frontend/src/containers/user/Login.test.tsx
+++ frontend/src/containers/user/Login.test.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { act } from 'react-dom/test-utils';
@@ -8,12 +8,12 @@ import Login from './Login';
 
 const mockNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
-  ...(jest.requireActual('react-router-dom') as any),
+  ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockNavigate,
 }));
 const mockDispatch = jest.fn();
 jest.mock('react-redux', () => ({
-  ...(jest.requireActual('react-redux') as any),
+  ...jest.requireActual('react-redux'),
   useDispatch: () => mockDispatch,
 }));
 beforeEach(() => jest.clearAllMocks());
@@ -42,5 +42,17 @@ describe('[Login Page]', () => {
       expect(mockNavigate).toBeCalledTimes(1);
       expect(mockNavigate).toBeCalledWith('/');
     });
+  });
+
+  test('login', () => {
+    setup();
+    const usernameInput = screen.getByPlaceholderText('ID');
+    const passwordInput = screen.getByPlaceholderText('Password');
+    const loginButton = screen.getByText('Login');
+    fireEvent.change(usernameInput, { target: { value: 'a' } });
+    fireEvent.change(passwordInput, { target: { value: 'b' } });
+    fireEvent.click(loginButton);
+    expect(mockDispatch).toBeCalledTimes(1);
+    expect(mockDispatch).toBeCalledWith({ type: 'user/login', payload: { username: 'a', password: 'b' } });
   });
 });
