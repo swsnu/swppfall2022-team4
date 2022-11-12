84cd60cb94d94a70db751fafbc125529ceccf6ee
blackruby02 <whiteruby02@snu.ac.kr>
2022-11-12 17:09:27 +0900

    [TEST] group container

---
+++ frontend/src/containers/group/GroupList.test.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { render, screen, fireEvent } from '@testing-library/react';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { act } from 'react-dom/test-utils';
+import { rootReducer } from 'store';
+import GroupList from './GroupList';
+import { initialState, getMockStore } from '../../test-utils/mocks';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockNavigate,
+}));
+const mockDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockDispatch,
+}));
+beforeEach(() => jest.clearAllMocks());
+afterAll(() => jest.restoreAllMocks());
+
+
+const setup = () => {
+  const store = configureStore({ reducer: rootReducer });
+  store.dispatch({
+    type: 'user/setUser',
+    payload: { username: 'username', nickname: 'nickname', image: 'image' },
+  });
+  render(
+    <Provider store={store}>
+      <GroupList />
+    </Provider>,
+  );
+  return store;
+};
+
+describe('setup test', () => {
+  it('test', () => {
+    setup();
+    expect(mockDispatch).toBeCalledTimes(2);
+  });
+  it('button', () => {
+    setup();
+    const createGroupBtn = screen.getByText('Create Group');
+    fireEvent.click(createGroupBtn);
+    expect(mockNavigate).toBeCalledTimes(1);
+    expect(mockNavigate).toBeCalledWith('/group/create');
+  })
+})
