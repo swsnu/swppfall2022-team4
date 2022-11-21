225a501353dbb23ec507fcb70daa94122d2a2eff
Kojihyung <kokoleerh@naver.com>
2022-11-21 22:34:20 +0900

    [FEAT] 종목 별로 구분

--- frontend/src/components/fitelement/Hover.tsx
+++ frontend/src/components/fitelement/Hover.tsx
@@ -1,14 +1,37 @@
 import styled from 'styled-components';
 
+type fitElementType = {
+  type: string;
+  workout_type: string;
+  period: number;
+  category: string;
+  weight: number;
+  rep: number;
+  set: number;
+  time: number;
+  date: Date | null;
+};
+
 export interface IProps {
   workouts: [];
 }
 
+let workouts_list = new Map<String, fitElementType[]>();
+
 export const Hover = (props: IProps) => {
-  let workouts_list = {};
+    if (typeof props.workouts !== 'undefined' && props.workouts?.length > 0) {
+    props.workouts?.map((workout: fitElementType) => {
+      if (workouts_list.get(workout['category']) !== undefined) {
+        workouts_list.get(workout['category'])!.push(workout);
+      } else {
+        workouts_list.set(workout['category'], []);
+      }
+    });
+        console.log(workouts_list)
+  }
   return (
-    <HoverWrapper>
-      {props.workouts?.map(workout => workout['workout_type'])}
+      <HoverWrapper>
+      {workouts_list.get('back')?.map(workout => workout['workout_type'])}
       <br />
       {props.workouts?.length} 종목
     </HoverWrapper>
