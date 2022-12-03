18e75b751b07541aeb6c54a97a8094520ca3fc10
blackruby02 <whiteruby02@snu.ac.kr>
2022-12-03 11:06:36 +0900

    [TEST] gr buttons

---
+++ frontend/src/components/group/GroupButton2.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import GroupButton2 from './GroupButton2';
+
+const onClick = jest.fn();
+beforeEach(() => jest.clearAllMocks());
+
+describe('<GroupButton2 />', () => {
+    it('Should render GroupButton2', () => {
+      render(
+        <GroupButton2 content="content" clicked={onClick} end={true}/>
+      );
+      const button = screen.getByText('content');
+      expect(button).toBeInTheDocument();
+      fireEvent.click(button);
+      expect(onClick).toHaveBeenCalledTimes(1);
+    });
+})
