36cc5697464d3e7b95df4fbd0ddc35d580ca6b27
blackruby02 <whiteruby02@snu.ac.kr>
2022-12-03 11:06:48 +0900

    [TEST] group element

--- frontend/src/components/group/GroupElement.test.tsx
+++ frontend/src/components/group/GroupElement.test.tsx
@@ -1,68 +1,80 @@
 import { render, screen } from '@testing-library/react';
 import { GroupElement } from './GroupElement';
+import { TagVisual } from 'store/apis/tag';
+
+const single_tag: TagVisual = {
+  id: '1',
+  name: '데드리프트',
+  color: '#dbdbdb'
+}
+
+const onClick = jest.fn();
+beforeEach(() => jest.clearAllMocks());
 
 describe('<GroupElement />', () => {
-  it('should render without errors1', () => {
+  it('All full', () => {
     render(
       <GroupElement
         id={1}
         group_name={'test'}
-        clicked={() => 0}
+        clicked={onClick}
+        number={12}
+        member_number={10}
+        start_date={'2022-01-01'}
+        end_date={'2023-12-31'}
+        address={'서울시 관악구 봉천동'}
+        free={true}
+        prime_tag={single_tag}
+      />,
+    );
+    screen.getByText('test');
+    screen.getByText('장소 : 서울시 관악구 봉천동');
+    screen.getByText('최대인원 : 12');
+    screen.getByText('현재인원 : 10');
+    screen.getByText('자유가입O');
+    screen.getByText('시작일 : 2022-01-01');
+    screen.getByText('마감일 : 2023-12-31');
+    screen.getByText('데드리프트');
+  });
+  it('long address & done', () => {
+    render(
+      <GroupElement
+        id={1}
+        group_name={'test'}
+        clicked={onClick}
         number={12}
         member_number={10}
         start_date={'2019-01-01'}
         end_date={'2019-12-31'}
-        address={'봉천동'}
+        address={'서울특별시 관악구 봉천동 대천에이스빌 505호'}
         free={true}
-        prime_tag={undefined}
+        prime_tag={single_tag}
       />,
     );
-    screen.getByText('test');
-    screen.getByText('시작일 : 2019-01-01');
-    screen.getByText('마감일 : 2019-12-31');
-    screen.getByText('최대인원 : 12');
-    screen.getByText('현재인원 : 10');
+    screen.getByText('장소 : 서울특별시 관악구 봉천동 대...');
   });
-  it('should render without errors2', () => {
+  it('all null', () => {
     render(
       <GroupElement
         id={1}
         group_name={'test'}
-        clicked={() => 0}
-        number={12}
+        clicked={onClick}
+        number={null}
         member_number={10}
         start_date={null}
         end_date={null}
-        address={'청룡동'}
-        free={true}
+        address={null}
+        free={false}
         prime_tag={undefined}
       />,
     );
     screen.getByText('test');
-    screen.getByText('시작일 : 기한없음');
-    screen.getByText('마감일 : 기한없음');
-    screen.getByText('최대인원 : 12');
-    screen.getByText('현재인원 : 10');
-  });
-  it('should render without errors3', () => {
-    render(
-      <GroupElement
-        id={1}
-        group_name={'test'}
-        clicked={() => 0}
-        number={null}
-        member_number={10}
-        start_date={'2019-01-01'}
-        end_date={'2019-12-31'}
-        address={'서울특별시 관악구 봉천동 875-4 대천에이스빌 505호'}
-        free={true}
-        prime_tag={undefined}
-      />,
-    );
-    screen.getByText('test');
-    screen.getByText('시작일 : 2019-01-01');
-    screen.getByText('마감일 : 2019-12-31');
+    screen.getByText('장소 : 장소없음');
     screen.getByText('최대인원 : 제한없음');
     screen.getByText('현재인원 : 10');
+    screen.getByText('자유가입X');
+    screen.getByText('시작일 : 기한없음');
+    screen.getByText('마감일 : 기한없음');
+    screen.getByText('None');
   });
 });
