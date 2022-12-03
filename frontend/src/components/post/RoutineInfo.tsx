d35df26dccffa7b86cb9a195b46f95aee38b15df
KJYoung <jykim157@snu.ac.kr>
2022-12-03 21:54:05 +0900

    [DESIGN] PostDetail with Routine

--- frontend/src/components/post/RoutineInfo.tsx
+++ frontend/src/components/post/RoutineInfo.tsx
@@ -4,11 +4,11 @@ import styled from 'styled-components';
 
 const RoutineItemGrid = styled.div`
   display: grid;
-  grid-template-columns: 5fr 10fr 20fr 8fr 8fr 8fr 8fr;
+  grid-template-columns: 3fr 15fr 20fr 8fr 8fr 8fr 8fr;
 `;
 
 const RoutineHeader = () => (
-  <RoutineItemGrid>
+  <RoutineItemGrid style={{ color: 'var(--fit-support-gray)' }}>
     <span></span>
     <span></span>
     <span>종류</span>
@@ -23,29 +23,30 @@ export const RoutineInfo = ({ routine }: { routine: RoutineTypeInPost | null | u
   if (routine) {
     return (
       <RoutineInfoWrapper>
-        <span>태그된 루틴</span>
+        <span>태그된 루틴 : {routine.name}</span>
         <div>
-          <span>{routine.name}</span>
           <div>
             <RoutineHeader />
-            {routine.fitelements.map((fitelement, index) => (
-              <RoutineItems key={index}>
-                <span>{index + 1}</span>
-                <div>
-                  <LogImage
-                    src={require(`assets/images/workout_log/fitelement_category/${get_image(
-                      fitelement.workout_type,
-                    )}.png`)}
-                  />
-                  {fitelement.workout_type}
-                </div>
-                <span>{fitelement.workout_name}</span>
-                <span>{fitelement.weight}</span>
-                <span>{fitelement.rep}</span>
-                <span>{fitelement.set}</span>
-                <span>{fitelement.time}</span>
-              </RoutineItems>
-            ))}
+            <RoutineItemsWrapper>
+              {routine.fitelements.map((fitelement, index) => (
+                <RoutineItems key={index}>
+                  <span>{index + 1}</span>
+                  <div>
+                    <img
+                      src={require(`assets/images/workout_log/fitelement_category/${get_image(
+                        fitelement.workout_type,
+                      )}.png`)}
+                    />
+                    <span>{fitelement.workout_type}</span>
+                  </div>
+                  <span>{fitelement.workout_name}</span>
+                  <span>{fitelement.weight}</span>
+                  <span>{fitelement.rep}</span>
+                  <span>{fitelement.set}</span>
+                  <span>{fitelement.time}</span>
+                </RoutineItems>
+              ))}
+            </RoutineItemsWrapper>
           </div>
         </div>
       </RoutineInfoWrapper>
@@ -55,13 +56,6 @@ export const RoutineInfo = ({ routine }: { routine: RoutineTypeInPost | null | u
   }
 };
 
-const LogImage = styled.img`
-  width: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
 const RoutineInfoWrapper = styled.div`
   width: 100%;
   background-color: var(--fit-white);
@@ -69,7 +63,6 @@ const RoutineInfoWrapper = styled.div`
   padding: 10px 15px;
   display: flex;
   flex-direction: column;
-  cursor: pointer;
 
   > span:first-child {
     /* 태그된 루틴 */
@@ -83,22 +76,38 @@ const RoutineInfoWrapper = styled.div`
     flex-direction: column;
     align-items: flex-start;
     padding: 5px 10px;
-    > span:first-child {
-      /* 루틴 이름 */
-      font-size: 18px;
-      font-weight: 500;
-      margin-bottom: 5px;
-    }
-    > div:nth-child(2) {
+    text-align: center;
+    > div:first-child {
       width: 100%;
+      > div:first-child {
+        border-bottom: 0.2px solid var(--fit-support-gray);
+        padding-bottom: 5px;
+        margin-bottom: 5px;
+      }
     }
   }
 `;
 
+const RoutineItemsWrapper = styled.div`
+  display: grid;
+  row-gap: 10px;
+`;
 const RoutineItems = styled(RoutineItemGrid)`
   align-items: center;
-  div {
+  > span:first-child {
+    color: var(--fit-support-gray);
+  }
+  > div:nth-child(2) {
     display: flex;
     align-items: center;
+    img {
+      width: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    span {
+      margin-left: 10px;
+    }
   }
 `;
