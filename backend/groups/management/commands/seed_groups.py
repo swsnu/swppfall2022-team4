c44b1906ccbc71b4b09397ce0061fec591d75c4d
blackruby02 <whiteruby02@snu.ac.kr>
2022-12-06 15:58:02 +0900

    [FEAT] cert & join seed

--- backend/groups/management/commands/seed_groups.py
+++ backend/groups/management/commands/seed_groups.py
@@ -1,5 +1,5 @@
 import random
-import datetime
+from datetime import datetime, timedelta
 
 from django.core.management import BaseCommand
 from django.contrib.admin.utils import flatten
@@ -29,11 +29,12 @@ class Command(BaseCommand):
                 "group_leader": lambda x: random.choice(all_users),
                 "number": lambda x: random.randint(50,100),
                 "start_date": lambda x: seeder.faker.date_between_dates(
-                    datetime.datetime(2022, 1, 1), datetime.datetime(2022, 7, 1)
+                    datetime(2022, 1, 1), datetime(2022, 7, 1)
                 ),
                 "end_date": lambda x: seeder.faker.date_between_dates(
-                    datetime.datetime(2022, 7, 2), datetime.datetime(2023, 12, 30)
+                    datetime(2022, 7, 2), datetime(2023, 12, 30)
                 ),
+                "member_number": lambda x: 0,
                 "description": lambda x: seeder.faker.sentence(),
                 "free": lambda x: bool(random.getrandbits(1)),
                 "lat": lambda x: random.randint(32,38),
@@ -44,16 +45,13 @@ class Command(BaseCommand):
 
         created_groups_ = seeder.execute()
         created_groups = flatten(list(created_groups_.values()))
+        
+        today = datetime.now().strftime("%Y-%m-%d")
 
         for group_pk in created_groups:
             group = Group.objects.get(pk=group_pk)
             join = JoinRequest(group=group)
             join.save()
-
-            #members add
-            for user in User.objects.order_by("?"):
-                if random.randint(1, 10) <= 1:
-                    group.members.add(user)
 
             #goal add
             for tag in Tag.objects.filter(tag_name='레그 프레스').all():
@@ -69,6 +67,18 @@ class Command(BaseCommand):
                         )
                     fit.save()
                     group.goal.add(fit)
+
+            #members add
+            for user in User.objects.order_by("?"):
+                if random.randint(1, 10) <= 1:
+                    group.members.add(user)
+                    group.member_number += 1
+                    cert = GroupCert(group=group, member=user, date=today)
+                    cert.save()
+                    cert.fit_element.add(group.goal.get(weight=100))
+                elif random.randint(1, 10) >= 9:
+                    if (group.free==False):
+                        join.members.add(user)
 
             #tags
             for tag in Tag.objects.all():
